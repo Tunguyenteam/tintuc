549ec319150fd1c3a9f18802bbe411eb55cf944b
cảnh <cnh@ASUS-PC>
2015-11-25 22:53:04 +0700

    canh commit

---
+++ src/com/tunguyen/tintin/ActivityDoc.java
@@ -0,0 +1,33 @@
+package com.tunguyen.tintin;
+
+import com.example.tin.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class ActivityDoc extends Activity {
+	MainActivity mainActivity = new MainActivity();
+	TextView tvtitledoc,tvcontentdoc;
+	public String titledoc,contentdoc;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_doc);
+		tvtitledoc = (TextView)findViewById(R.id.titledoc);
+		tvcontentdoc = (TextView)findViewById(R.id.contentdoc);
+		getActionBar().setTitle(MainActivity.titleactionbar);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		Intent intentread = getIntent();
+		tvtitledoc.setText(intentread.getStringExtra("title"));
+		tvcontentdoc.setText(intentread.getStringExtra("content"));
+		
+	}
+	public boolean onOptionsItemSelected(MenuItem item) {
+		finish();
+		return super.onOptionsItemSelected(item);
+	}
+
+}
